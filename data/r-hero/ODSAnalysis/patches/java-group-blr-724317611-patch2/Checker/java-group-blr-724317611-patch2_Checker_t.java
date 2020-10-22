428b8941dd9cbba4a73e017bb3e64d928745c049
SophieHYe <he_ye_90s@hotmail.com>
2020-10-22 08:15:20 +0200

    added ods analysis

---
+++ data/r-hero/ODSAnalysis/patches/java-group-blr-724317611-patch2/Checker/java-group-blr-724317611-patch2_Checker_t.java
@@ -0,0 +1,12 @@
+package com.company.util;
+
+public class Checker {
+    public boolean isEven(int number) {
+       return (number%2==0);
+
+    }
+
+    public boolean isOdd(int number) {
+        return(number%number)&0;
+    }
+}
